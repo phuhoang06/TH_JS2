391009482b4af911c91da07113a336845abc2c1e
phuhoang.06 <hoangvanphu2006@gmail.com>
2024-10-08 14:21:35 +0700

    add

---
+++ index.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputHeight = prompt("nhập vào Height");
+inputWidth = prompt("nhập vào Width");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích : " + area);
